a5353f7ce614ca8a7e0ec5f9f3338f8fe95cc5dc
Reygenz <reyvaldowildan7298@gmail.com>
2023-12-13 00:41:47 +0700

    login

--- resources/views/layouts/head-css.blade.php
+++ resources/views/layouts/head-css.blade.php
@@ -5,10 +5,8 @@
 <link href="{{ URL::asset('build/css/bootstrap.min.css') }}"  rel="stylesheet" type="text/css" />
 <!-- Icons Css -->
 <link href="{{ URL::asset('build/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
-<!-- App Css -->
+<!-- App Css-->
 <link href="{{ URL::asset('build/css/app.min.css') }}"  rel="stylesheet" type="text/css" />
-<!-- custom Css -->
+<!-- custom Css-->
 <link href="{{ URL::asset('build/css/custom.min.css') }}"  rel="stylesheet" type="text/css" />
-<!-- AOS Css -->
-<link rel="stylesheet" href="{{ URL::asset('build/libs/aos/aos.css')}}" type="text/css"/>
 {{-- @yield('css') --}}
--- resources/views/layouts/vendor-scripts.blade.php
+++ resources/views/layouts/vendor-scripts.blade.php
@@ -4,13 +4,5 @@
 <script src="{{ URL::asset('build/libs/feather-icons/feather.min.js') }}"></script>
 <script src="{{ URL::asset('build/js/pages/plugins/lord-icon-2.1.0.js') }}"></script>
 <script src="{{ URL::asset('build/js/plugins.js') }}"></script>
-<script src="{{ URL::asset('build/libs/aos/aos.js')}}"></script>
-<script src="{{ URL::asset('build/js/pages/animation-aos.init.js')}}"></script>
-<script>
-    AOS.init({
-        easing: 'ease-out-back',
-        duration: 1000
-    });
-</script>
 @yield('script')
 @yield('script-bottom')
